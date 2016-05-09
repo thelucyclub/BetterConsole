6d9a09e5b73f9b14e8f07341d0249c829741a8a8
TheLucyClub Github Team <thelucyclub@gmail.com>
2016-05-09 07:08:27 -0400

    Add files via upload

---
+++ better console .cmd
@@ -0,0 +1,1 @@
+start "" bin\betterConsole.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "Nukkit BetterConsole" bin\reader.bat --enable-ansi %*
---
+++ reader.bat
@@ -0,0 +1,4 @@
+cd ..
+set /p JAR="Nukkit Jar (i.e. nukkit.jar): "
+java -jar %JAR%
+exit 257
